76bfe34d603afa73714a428d4934e71e69c5d0ed
AnClicks <ankitcode1234@gmail.com>
2025-03-30 02:50:51 +0530

    Database COnnection established

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: '.env'}) //tradition way to load env files
+
+
+import dotenv from "dotenv"
+import  connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+// console.log(process.env.MONGO_URI)
+connectDB()
+
+
+
+
+
+
+
+//good aproach but the index in populated too much
+// import express from "express"
+// const app = express()
+
+// (async () => {
+//     try {
+//          await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{ //appication not able to talk to the database
+//             console.log("ERR: ",error);
+//             throw error
+//         })
+//         app.listend(process.env.PORT,()=>{
+//             console.log(`App is listning on port ${process.env.PORT}`)
+//         })
+//      }
+//     catch (error){
+//         console.error("ERROR:",error)
+//         throw err
+//     }
+// })()
