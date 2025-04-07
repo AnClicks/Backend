e7b9efe0ed5ed7968a4a3a6dbdd503263406c89e
AnClicks <ankitcode1234@gmail.com>
2025-04-08 00:25:09 +0530

    Registration of user

--- src/app.js
+++ src/app.js
@@ -10,8 +10,18 @@ app.use(expres.json({limit:"16kb"})) //json ka data
 app.use(expres.urlencoded({extended: true, limit:"16kb"})) // url data
 app.use(expres.static("public"))
 app.use(cookieParser())
-
 //middleware
+
+app.get("/",(req,res)=>{
+    res.send("Server is running")
+})
+//router
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration //we could have user app.get to perform any action but controllers are else where so use the fuction like middlewares
+app.use("/ api/v1/users",userRouter)
+
 
 
 export { app }
